30d01358ca2d07ca0ec25446521fe1655e654a2b
Chris McDermut <chrismcdermut@gmail.com>
2026-03-23 12:45:02 -0700

    feat(PRLT-1074): replace better-sqlite3 imports with sql.js in newly merged files (ship.ts, state-resolution.ts)

--- apps/cli/src/commands/work/ship.ts
+++ apps/cli/src/commands/work/ship.ts
@@ -1,7 +1,7 @@
 import { Args, Flags } from '@oclif/core';
 import * as path from 'node:path';
 import * as fs from 'node:fs';
-import Database from 'better-sqlite3';
+import { SqliteDatabase } from '../../lib/database/sqlite.js';
 import { PMOCommand, pmoBaseFlags, autoExportToBoard } from '../../lib/pmo/index.js';
 import { getWorkColumnSetting, findColumnByName } from '../../lib/pmo/utils.js';
 import { styles } from '../../lib/styles.js';
@@ -123,7 +123,7 @@ export default class WorkShip extends PMOCommand {
     }
 
     const dbPath = path.join(workspaceInfo.path, '.proletariat', 'workspace.db');
-    const db = new Database(dbPath);
+    const db = new SqliteDatabase(dbPath);
     const executionStorage = new ExecutionStorage(db);
 
     try {
